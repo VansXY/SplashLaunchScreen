81431bee27cc8556d5b8b186f5792005815668c3
肖扬 <xy_dtb@163.com>
2018-06-01 16:02:53 +0800

    commit XYPlayerLayer

---
+++ SplashLaunchScreenVC/SplashLaunchScreenVC/XYPlayer/XYPlayerLayer.h
@@ -0,0 +1,13 @@
+//
+//  XYPlayerLayer.h
+//  SplashLaunchScreenVC
+//
+//  Created by HXB-xiaoYang on 2018/6/1.
+//Copyright © 2018年 Vickate. All rights reserved.
+//
+
+#import <AVFoundation/AVFoundation.h>
+
+@interface XYPlayerLayer : AVPlayerLayer
+
+@end
---
+++ SplashLaunchScreenVC/SplashLaunchScreenVC/XYPlayer/XYPlayerLayer.m
@@ -0,0 +1,13 @@
+//
+//  XYPlayerLayer.m
+//  SplashLaunchScreenVC
+//
+//  Created by HXB-xiaoYang on 2018/6/1.
+//Copyright © 2018年 Vickate. All rights reserved.
+//
+
+#import "XYPlayerLayer.h"
+
+@implementation XYPlayerLayer
+
+@end
